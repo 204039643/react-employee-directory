cdb589bda836a11a034e86423967f5fb4007e62f
Justin Sykes <atlsykes1@att.net>
2021-01-24 15:29:45 -0500

    small update to push

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Row from '../row/row'
 
 
-const Table = (props) => {
+const Table = ({handleSort, sortedResults}) => {
   return (
     <div>
       <br />
       <div className='row'>
         <div className='col s6 centered'>
-        <a className='waves-effect waves-light btn-small'
-          onClick={props.handleSort}>
-            <i className='material-icons'>sort_by_alpha</i>  Sort Last Name</a>
+        <a className='waves-effect waves-light btn-small' href="/"
+          onClick={handleSort}>
+            <i className='material-icons'>sort_by_alpha</i>  Sort by Email</a>
         </div>
         <form className='col s6 centered'>
           <div className='input-field col s6'>
@@ -20,15 +21,30 @@ const Table = (props) => {
         </form>
       </div>
       <table>
-        <thead>
+        <thead className='tdCSS'>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Location</th>
             <th>Email</th>
+            <th>Gender</th>
           </tr>
         </thead>
+        <tbody>
+        {sortedResults.map(result => (
+        <Row
+          key={result.login.uuid}
+          imageURL={result.picture.thumbnail}
+          firstName={result.name.first}
+          lastName={result.name.last}
+          location={result.location.country}
+          email={result.email}
+          gender={result.gender}
+        />
+      ))}
+        </tbody>
       </table>
+
     </div>
   )
 }
